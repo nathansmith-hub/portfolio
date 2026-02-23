5eb2941f43efe02fda3e59a74ecf6ca3992df365
nathansmith-hub <nathanhsmith@live.com>
2026-02-23 17:03:12 +1300

    getting ready to push to internet

--- src/gencontent.py
+++ src/gencontent.py
@@ -20,7 +20,7 @@ def extract_title(markdown):
         
     raise Exception("markdown must have a title")
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
 
     with open(from_path) as file:
@@ -34,18 +34,20 @@ def generate_page(from_path, template_path, dest_path):
 
     html_page = template.replace("{{ Title }}", title)
     html_page = html_page.replace("{{ Content }}", html_content)
+    html_page = html_page.replace('href="/', f'href="{basepath}')
+    html_page = html_page.replace('src="/', f'src="{basepath}')
 
     os.makedirs(os.path.dirname(dest_path), exist_ok=True)
 
     with open(dest_path, "w") as file:
         file.write(html_page)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
         if os.path.isfile(from_path) == True:
             dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path)
+            generate_page(from_path, template_path, dest_path, basepath)
         else:
-            generate_pages_recursive(from_path, template_path, dest_path)
+            generate_pages_recursive(from_path, template_path, dest_path, basepath)
--- src/main.py
+++ src/main.py
@@ -1,21 +1,29 @@
 import os
+import sys
+import shutil
 
 from copystatic import copy_static
 
 from gencontent import generate_pages_recursive
                
 def main():
+    basepath = "/"
+    if len(sys.argv) > 1:
+        basepath = sys.argv[1]
+        
     repo_root = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     static = os.path.join(repo_root, "static")
-    public = os.path.join(repo_root, "public")
+    docs = os.path.join(repo_root, "docs")
 
-    copy_static(static, public)
+    if os.path.exists(docs):
+        shutil.rmtree(docs)
+
+    copy_static(static, docs)
 
     source_index = os.path.join(repo_root, "content")
     template = os.path.join(repo_root, "template.html")
-    dest_index = os.path.join(repo_root, "public")
 
-    generate_pages_recursive(source_index, template, dest_index)
+    generate_pages_recursive(source_index, template, docs, basepath)
 
 if __name__ == "__main__":
     main()
